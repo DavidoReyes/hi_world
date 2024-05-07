e4ba25b4d002d86fdb4daf3e7dc6cff3921ea954
David Reyes <david-reyes-torres@outlook.com>
2024-05-07 13:47:39 -0600

    Full!!!

--- main.py
+++ main.py
@@ -2,3 +2,10 @@
 @author:David Reyes
 """
 print("Hi World\nMy first code!!!")
+
+# testDR
+#numBegin = 0
+numStop = 5
+for numBegin in range(0,numStop):
+    print(f"numero: {numBegin}")
+    numBegin + 1
